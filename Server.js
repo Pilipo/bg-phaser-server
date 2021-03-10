cc231e72af86ba44a17b875b74733e191152c2a6
Phillip <pl@philliplehner.com>
2021-03-10 17:07:01 -0600

    Got it all working

--- Server.js
+++ Server.js
@@ -1,6 +1,11 @@
 const { Server } = require('boardgame.io/server');
 const { TTT } = require('./src/game');
 
+const lobbyConfig = {
+  apiPort: 8080,
+  apiCallback: () => console.log('Running Lobby on port 8080'),
+}
+
 const server = Server({ games: [TTT] });
 
-server.run(8000);
+server.run({ port: 8000, lobbyConfig });
